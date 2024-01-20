17ea7e6fc91ba14585032e0f8e4a9c6ac2eed11b
blah148 <info@ploddings.com>
2024-01-19 23:35:06 -0700

    ok it's setup.. the retrieval

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const session = require('express-session');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const app = express(); // Initialize instance of Express app for HTTP requests
@@ -38,6 +39,28 @@ app.use(passport.initialize());
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'task_planner_react', 'build')));
+
+
+// Function to encrypt data
+function encrypt(text, secretKey) {
+    const iv = crypto.randomBytes(16); // Initialization vector
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+
+// Function to decrypt data
+function decrypt(text, secretKey) {
+    let textParts = text.split(':');
+    let iv = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
 
 /* 
 - App.listen(): On the 'app' instance of the Express application, method listens on specified host & port
@@ -261,8 +284,12 @@ app.post('/tasks/new', verifyJWT, insertTaskMiddleware, retrieveTaskId);
 // Middleware function for inserting a task
 async function insertTaskMiddleware(req, res, next) {
     try {
-        const { start_time, end_time, task_description, isComplete, display_none, visibility } = req.body;
+        let { start_time, end_time, task_description, isComplete, display_none, visibility } = req.body;
         const user_id = req.user.sub; // Replace 'sub' with the appropriate field from your JWT payload
+
+        // Encrypt the task_description
+        const secretKey = process.env.CRYPTO_KEY; // stored in .env
+        const encryptedTaskDescription = encrypt(task_description, secretKey);
 
         // Use Supabase client to insert a new task
         const { error } = await supabase
@@ -270,7 +297,7 @@ async function insertTaskMiddleware(req, res, next) {
             .insert({
                     start_time: start_time,
                     end_time: end_time,
-                    task_description: task_description,
+                    task_description: encryptedTaskDescription,
                     is_complete: isComplete,
                     display_none: display_none,
                     visibility: visibility,
